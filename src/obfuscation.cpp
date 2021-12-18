c1e3bb1ade8588a5bbcf69ce8c8ad04aee9a1f20
RHshakil00 <35897130+RHshakil00@users.noreply.github.com>
2021-12-18 08:39:05 +0600

    Update obfuscation.cpp

--- src/obfuscation.cpp
+++ src/obfuscation.cpp
@@ -535,7 +535,7 @@ bool CObfuScationSigner::IsVinAssociatedWithPubkey(CTxIn& vin, CPubKey& pubkey)
     uint256 hash;
     if (GetTransaction(vin.prevout.hash, txVin, hash, true)) {
         for (CTxOut out : txVin.vout) {
-            if (out.nValue == 10000 * COIN) {
+            if (out.nValue == 200000 * COIN) {
                 if (out.scriptPubKey == payee2) return true;
             }
         }

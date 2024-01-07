f6fc26f416626eed8dac57fa45a57f2d1d9b954d
Funny Coder <35897130+encrypt4d@users.noreply.github.com>
2024-01-07 12:32:19 +0600

    Add files via upload

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -456,7 +456,7 @@ vector<COutput> CActiveMasternode::SelectCoinsMasternode()
 
     // Filter
     for (const COutput& out : vCoins) {
-        if (out.tx->vout[out.i].nValue == 200000 * COIN) { //exactly
+        if (out.tx->vout[out.i].nValue == 5000000 * COIN) { //exactly
             filteredCoins.push_back(out);
         }
     }
